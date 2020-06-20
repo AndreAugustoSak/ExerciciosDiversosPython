2a70d25622237e2c24ad1d319a0e13399aac4daf
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-06-19 22:52:20 -0300

    Update Questao_08.py

--- Questao_08.py
+++ Questao_08.py
@@ -1,6 +1,6 @@
 def passa_pela_janela(a, b, c, h, l):
     if a <= h and b <= l or a <= l and b <= h or a <= h and c <= l or a <= l and c <= h or b <= h and c <= l or b <= l and c <= h:
-                return True
+        return True
     else:
         return False
 
